5695d1ee8439bde3efb4912085d4546dd633e8c5
Daniel Smolentsev <daniel@dgtlworkshop.com>
2022-08-10 16:49:20 -0700

    Added adding and removing boards

--- src/lib/components/Row.svelte
+++ src/lib/components/Row.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	import type { BoardRow } from '$lib/interfaces/ConfigSchema';
 	export let row: BoardRow;
+	export let id: number;
+	row.id = id;
+	export let removeFunc: void;
 </script>
 <!-- <pre>{JSON.stringify(row)}</pre> -->
 <p>
 	<span>
-		CA1 <label>x: <input bind:value={row.x} type="number" /> </label><label>y: <input type="number" /> </label>
+		CA{id}
+		<label>x: <input bind:value={row.x} type="number" /> </label>
+		<label>y: <input bind:value={row.y} type="number" /> </label>
+		<label>width: <input bind:value={row.width} type="number" /> </label>
+		<label>height: <input bind:value={row.height} type="number" /> </label>
+		<button on:click={() => removeFunc(id)}>remove</button>
 	</span>
 </p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,40 @@
 <script lang="ts">
 	import Row from '$lib/components/Row.svelte';
+	import type { BoardRow } from '$lib/interfaces/ConfigSchema';
 	import { config } from '$lib/samples/sample-config';
 
 	let currentConfig = config;
+	let defaultBoardRow: BoardRow = {
+		id: 0,
+		x: 10,
+		y: 20,
+		width: 6,
+		height: 6,
+		rule: 0
+	};
+	function addCA() {
+		// add a board to json
+        let newRow = {...defaultBoardRow};
+		currentConfig.boards.push(newRow);
+        // console.log("addCA clicked");
+        // trigger reactivity
+        currentConfig = currentConfig;
+	}
+    function removeCA(id: number) {
+        // Remove element in with the id
+        currentConfig.boards.splice(id,1);
+        //console.log(currentConfig.boards.splice(id,1));
+        currentConfig = currentConfig;
+        console.log("removeCA"+id);
+
+    }
 </script>
 
 <h1>CA JSON Layout Editor</h1>
-{#each currentConfig.boards as this_board }
-    <Row bind:row={this_board} />
+{#each currentConfig.boards as this_board, i}
+	<Row bind:row={this_board} id={i} removeFunc={removeCA}/>
 {/each}
+<button on:click={addCA}>Add CA</button>
 
-<pre>{JSON.stringify(currentConfig, undefined, "  ")}</pre>
+<pre>{JSON.stringify(currentConfig, undefined, '  ')}</pre>
 <a href="./sidepage">Go to side page</a>
