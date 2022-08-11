467ba99a680731241b3b0f98f8c1d2546b604631
Daniel Smolentsev <daniel@dgtlworkshop.com>
2022-08-10 17:00:32 -0700

    fixed removeFunc type

---
+++ src/lib/components/SimSettings.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+
+</script>
+<h3>Sim Settings</h3>
+<span>
+    <label>Cell Size:<input type="number" value="4"></label>
+    <label>Run duration:<input type="number" value="4000"></label>
+</span>
